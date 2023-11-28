1d24fb5b1ac726d5a1c63991ab34a8005588a0ba
Linus Hoja <58180468+LinusHojaGER@users.noreply.github.com>
2023-11-28 23:33:08 +0100

    Initial commit

---
+++ src/main/java/net/alphalightning/paper/template/Plugin.java
@@ -0,0 +1,17 @@
+package net.alphalightning.paper.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
